e59a686ee7a025737fe4df41d402e8cebe3203be
Stef Heyenrath <stef.heyenrath@gmail.com>
2022-09-14 21:45:01 +0200

    .

--- examples/Replicate.Net.ConsoleApp/Program.cs
+++ examples/Replicate.Net.ConsoleApp/Program.cs
@@ -3,22 +3,56 @@ using Replicate.Net.Client;
 using Replicate.Net.Factory;
 using Replicate.Net.Models.Predictions;
 
-IPredictionsApiFactory factory = new PredictionsApiFactory();
+var factory = new PredictionsApiFactory();
 
-var client = factory.GetClient(Environment.GetEnvironmentVariable("replicate_token")!);
+var version = "a9758cbfbd5f3c2094457d996681af52552901775aa2d6dd0b17fd15df959bef";
+var prompt = "a gentleman cat with blue eyes wearing a tophat in a 19th century portrait";
 
-var request = new Request
+// await RunOnLocalAsync(factory).ConfigureAwait(false);
+
+await RunOnPredictionsAsync(factory).ConfigureAwait(false);
+
+// -------------------------------------------------------------------------------------------------------------
+
+async Task RunOnLocalAsync(IPredictionsApiFactory factory)
 {
-    Version = "a9758cbfbd5f3c2094457d996681af52552901775aa2d6dd0b17fd15df959bef",
-    Input = new Input
+    var client = factory.GetClient("https://localhost:5000");
+
+    var request = new Request
     {
-        Prompt = "a gentleman cat with blue eyes wearing a tophat in a 19th century portrait"
-    }
-};
+        Version = version,
+        Input = new Input
+        {
+            Prompt = prompt
+        }
+    };
 
-var predictions = await client.GetPredictionsAsync();
-Console.WriteLine("predictions = {0}", predictions.Results?.Count);
+    var predictions = await client.GetPredictionsAsync().ConfigureAwait(false);
+    Console.WriteLine("predictions = {0}", predictions.Results?.Count);
 
-var response = await client.CreatePredictionAndWaitOnResultAsync(request).ConfigureAwait(false);
+    var response = await client.CreatePredictionAndWaitOnResultAsync(request).ConfigureAwait(false);
 
-Console.WriteLine(JsonConvert.SerializeObject(response, Formatting.Indented));
+    Console.WriteLine(JsonConvert.SerializeObject(response, Formatting.Indented));
+}
+
+
+async Task RunOnPredictionsAsync(IPredictionsApiFactory factory)
+{
+    var client = factory.GetClient(Environment.GetEnvironmentVariable("replicate_token")!);
+
+    var request = new Request
+    {
+        Version = version,
+        Input = new Input
+        {
+            Prompt = prompt
+        }
+    };
+
+    var predictions = await client.GetPredictionsAsync().ConfigureAwait(false);
+    Console.WriteLine("predictions = {0}", predictions.Results?.Count);
+
+    var response = await client.CreatePredictionAndWaitOnResultAsync(request).ConfigureAwait(false);
+
+    Console.WriteLine(JsonConvert.SerializeObject(response, Formatting.Indented));
+}
--- src/Replicate.Net/Factory/PredictionsApiFactory.cs
+++ src/Replicate.Net/Factory/PredictionsApiFactory.cs
@@ -8,23 +8,21 @@ public class PredictionsApiFactory : IPredictionsApiFactory
 {
     private const string PredictionUrl = "https://api.replicate.com/v1/predictions";
 
-    public IPredictionsApi GetClient(string url, string token)
+    public IPredictionsApi GetClient(string url, string? token)
     {
         var client = new RestClient(Guard.NotNullOrEmpty(url)).For<IPredictionsApi>();
-        client.Token = Guard.NotNullOrEmpty(token);
-        return client;
-    }
-
-
-    public IPredictionsApi GetClient(string? token = null)
-    {
-        var client = new RestClient(PredictionUrl).For<IPredictionsApi>();
-
+        
         if (!string.IsNullOrEmpty(token))
         {
             client.Token = token;
         }
 
         return client;
+    }
+
+
+    public IPredictionsApi GetClient(string? token = null)
+    {
+        return GetClient(PredictionUrl, token);
     }
 }
